5777cc8e68924f73bc1d0ec4d158bcad941b91b2
Milos Milinkov <milos300399@gmail.com>
2023-03-03 22:50:24 +0100

    Product extended with Type and brand

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,8 +1,17 @@
 namespace Core.Entities
 {
-    public class Product
+    public class Product:BaseEntity
     {
-        public int Id { get; set; }
-        public string ProductName { get; set; }         
+        public string Name { get; set; }      
+        public string Description { get; set; }        
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        //ProductType and ProductBrand will help EntityFram to better migrate data 
+        //and auto create relations bassed on each Id
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+
+        public ProductBrand ProductBrand{ get; set; }
+        public int ProductBrandId { get; set; }
     }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,11 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
+using System.Reflection;
 
 namespace Infrastructure.Data
 {
     public class StoreContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
         public StoreContext(DbContextOptions options) : base(options)
         {
             
