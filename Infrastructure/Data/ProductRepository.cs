e91ca20c0e8575dafa88f7474255fa1e62f9d1a8
Milos Milinkov <milos300399@gmail.com>
2023-03-05 21:00:14 +0100

    Fixed: NULL on Product FKs

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -31,12 +31,18 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await _storeContext.Products.FindAsync(id);
+            return await _storeContext.Products
+                .Include(p=>p.ProductType)
+                .Include(p=>p.ProductBrand)
+                .FirstOrDefaultAsync(p=>p.Id==id);
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _storeContext.Products.ToListAsync();
+            return await _storeContext.Products
+                .Include(p=>p.ProductType)
+                .Include(p=>p.ProductBrand)
+                .ToListAsync();
         }
 
         public async Task<ProductType> GetTypeByIdAsync(int id)
