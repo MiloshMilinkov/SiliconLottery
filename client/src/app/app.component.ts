7ad187ead9d8a49bba2a57c9fbbd23146783d9d5
Milos Milinkov <milos300399@gmail.com>
2023-05-07 16:56:47 +0200

    nav-bar component, Http client (get all products)

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Silicon Lottery';
+  products:any[] =[];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response:any) => this.products=response.data, //what to do next
+      error: error => console.log(error),//what to do if there is an error
+      complete: () => {
+        console.log('request completed');
+      }
+    })
+  }
 }
