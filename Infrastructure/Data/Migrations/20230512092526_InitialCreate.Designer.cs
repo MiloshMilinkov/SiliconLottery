c3e4134b04c5afb7ebdc9d43ce97cfed62f0754b
Milos Milinkov <milos300399@gmail.com>
2023-05-12 13:03:25 +0200

    Adde more data into the db

--- Infrastructure/Data/Migrations/20230512092526_InitialCreate.Designer.cs
+++ Infrastructure/Data/Migrations/20230512092526_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20230303214607_InitialCreate")]
+    [Migration("20230512092526_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -38,7 +38,7 @@ namespace Infrastructure.Data.Migrations
                         .IsRequired()
                         .HasColumnType("TEXT");
 
-                    b.Property<decimal>("Price")
+                    b.Property<double>("Price")
                         .HasColumnType("decimal(18,2)");
 
                     b.Property<int>("ProductBrandId")
