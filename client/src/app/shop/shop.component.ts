aef5fe230f4bb9a466570484b8847cc009fa486d
Milos Milinkov <milos300399@gmail.com>
2023-05-12 13:02:35 +0200

    Styling,Sort and Filter function

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/product';
 import { ShopService } from './shop.service';
+import { Type } from '../shared/models/type';
+import { Brand } from '../shared/models/brand';
 
 @Component({
   selector: 'app-shop',
@@ -8,13 +10,39 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  products:Product[] =[];
+  products:Product[] = [];
+  brands  :Brand[]   = [];
+  types   :Type[]    = [];
+  brandIdSelected=0;
+  typeIdSelected=0;
+  sortSelected='name';
+  sortOptions=[
+    {name:'A-z: Low to high',value:'nameAsc'},
+    {name:'A-z: High to low',value:'nameDesc'},
+    {name:'Price: Low to high',value:'priceAsc'},
+    {name:'Price: High to low',value:'priceDesc'}
+  ];
 
   constructor(private shopServiceL:ShopService){}
 
   ngOnInit(): void {
-    this.shopServiceL.getProducts().subscribe({
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts(){
+    this.shopServiceL.getProducts(this.brandIdSelected,this.typeIdSelected,this.sortSelected).subscribe({
       next: response => this.products=response.data,
+      error: error => console.log(error),
+      complete: () => {
+        console.log('request completed');
+      }
+    })}
+
+  getBrands(){
+    this.shopServiceL.getBrands().subscribe({
+      next: response => this.brands=[{id:0,name:'All'},...response],
       error: error => console.log(error),
       complete: () => {
         console.log('request completed');
@@ -22,4 +50,28 @@ export class ShopComponent implements OnInit {
     })
   }
 
+  getTypes(){
+    this.shopServiceL.getTypes().subscribe({
+      next: response => this.types=[{id:0,name:'All'},...response],
+      error: error => console.log(error),
+      complete: () => {
+        console.log('request completed');
+      }
+    })
+  }
+
+  onBrandSelected(brandId:number){
+    this.brandIdSelected=brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId:number){
+    this.typeIdSelected=typeId;
+    this.getProducts();
+  }
+
+  onSortSelected(event:any){
+    this.sortSelected=event.target.value;
+    this.getProducts();
+  }
 }
