8aab8ea5bb2a4203c7fc76dc4a24292d3fcbf2e5
Milos Milinkov <milos300399@gmail.com>
2023-05-13 15:12:40 +0200

    Link routing , Returning a single product info

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -43,4 +43,8 @@ export class ShopService {
   getTypes(){
     return this.http.get<Type[]>(this.baseUrl+'products/types');
   }
+
+  getProduct(id:number){
+    return this.http.get<Product>(this.baseUrl+'products/'+id)
+  }
 }
